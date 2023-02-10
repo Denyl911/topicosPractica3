f1cc59b7ec78b026a3fc5eeb018e6674d9e43781
Denyl911 <imdenyl@gmail.com>
2023-02-09 23:28:02 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# topicosPractica3
---
+++ src/main/java/com/mycompany/practica3/Practica3.java
@@ -0,0 +1,12 @@
+package com.mycompany.practica3;
+
+/**
+ *
+ * @author denyl
+ */
+public class Practica3 {
+
+    public static void main(String[] args) {
+        new Ventana().setVisible(true);
+    }
+}
